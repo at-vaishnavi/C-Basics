5562d4a3a40a365e87e43c119c8d4622713dcc9c
Vaishnavi V <192493313+at-vaishnavi@users.noreply.github.com>
2026-03-15 20:56:20 +0530

    Create calci.c

---
+++ calculator/calci.c
@@ -0,0 +1,46 @@
+// Simple Calculator using Conditional Statements
+#include<stdio.h>
+int main()
+{
+  char op;
+  int a ,b,result;
+  printf("Enter the valid operator:");
+  scanf("%c",&op);
+  if(op=='+' || op=='-' || op=='*' ||op=='/')
+  {
+   printf("Enter VAlues for a and b:");
+   scanf("%d%d",&a,&b);
+   if(op=='+') 
+    {
+      result=a+b;
+      printf("Addition of a and b:%d",result);
+    }
+    else if(op=='-') 
+    {
+      result=a-b;
+      printf("Differnece of a and b:%d",result);
+    }
+    else if(op=='*') 
+    {
+      result=a*b;
+      printf("Product of a and b:%d",result);
+    }
+    else if (op=='/')
+    {
+      if(b==0)
+      {
+        printf("Zero Division Erro");
+      }
+      else
+      {
+      result=a/b;
+      printf("Division of a and b:%d",result);
+      }
+    }
+  }
+ else 
+ {
+  printf("Invalid Operator !");
+ }
+return 0;
+  }
